599c4dcb025a7b47abc7e179e4a1102232c1e33d
Umar Shabazov <umarshabazov97@gmail.com>
2026-03-07 14:02:41 +0300

    storage: refactor storage layer

--- src/main/java/com/example/file_storage/repository/ResourceRepository.java
+++ src/main/java/com/example/file_storage/repository/ResourceRepository.java
@@ -1,8 +1,24 @@
 package com.example.file_storage.repository;
 
 import com.example.file_storage.entity.ResourceEntity;
+import com.example.file_storage.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ResourceRepository extends JpaRepository <ResourceEntity, Long> {
 
+    Optional<ResourceEntity> findByOwnerAndFullPath(UserEntity owner, String path);
+
+    void deleteByOwnerAndFullPath(UserEntity owner, String path);
+
+    List<ResourceEntity> findAllByOwnerAndFullPathStartingWith(UserEntity owner, String path);
+
+    Page<ResourceEntity> findAllByOwnerAndNameContainingIgnoreCase(UserEntity owner, String query, Pageable pageable);
+    List<ResourceEntity> findAllByOwnerAndParentPath(UserEntity owner, String parentPath);
+
+    boolean existsByOwnerAndFullPath(UserEntity owner, String path);
 }
